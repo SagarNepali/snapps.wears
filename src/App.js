f4ea1a716be7c92b6c3a9d391af8c77cb20e034f
Sagar <worthless.dumboo@gmail.com>
2021-05-09 19:44:39 +0545

    Router used for hats section for navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import HomePage from './pages/homepage/homepage.component'
 import '../src/App.css'
+import{Router,Switch, Route } from 'react-router-dom'
+import HatsPage from './pages/hats/hatspage'
 
 function App() {
   return (
     <div className="App">
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/hats' component={HatsPage}/>
+      </Switch>
     </div>
   )
 }
