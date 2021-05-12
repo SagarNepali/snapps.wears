1aa701a5f67a2bae6e34b6ecadf2c84dee2d0caa
Sagar <worthless.dumboo@gmail.com>
2021-05-12 20:35:41 +0545

    Singup credential used for signing in a user from firestore database

--- src/component/sign-in/sign-in.jsx
+++ src/component/sign-in/sign-in.jsx
@@ -4,7 +4,7 @@ import CustomButton from '../custom-button/custom-button';
 import FormInput from '../form-input/form-input'
 import './sign-in.scss'
 
-import {signInWithGoogle} from '../../firebase/firebase.utils'
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils'
 
 class SignIn extends React.Component {
 
@@ -17,10 +17,19 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        this.setState({email:'', password:""})
+        const {email,password} = this.state;
+
+        try{
+            await auth.signInWithEmailAndPassword(email,password)
+            this.setState({email:'', password:""})
+        }catch(error){
+            console.log(error.message)
+        }
+
+        
     }
 
     handleChange = event => {
